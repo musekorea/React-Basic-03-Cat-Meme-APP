ccfb36fd5b3576deb4e61810713a77dbba76160d
musekorea <musekorea@hanmail.net>
2022-02-03 17:56:52 +0800

    cat API

--- app.js
+++ app.js
@@ -1,9 +1,76 @@
 const root = document.querySelector('#root');
 
-const App = () => {
+const fetchAPI = async (inputValue) => {
+  try {
+    const apiFetch = await fetch(
+      `https://cataas.com/cat/says/${inputValue}?json=true`
+    );
+    const apiJSON = await apiFetch.json();
+    const catMeme = 'https://cataas.com' + apiJSON.url;
+    console.log(catMeme);
+    return catMeme;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const characterValidation = (inputValue) => {
+  if (inputValue === '') {
+    return 'Empty';
+  } else if (/[ㄱ-ㅎㅏ-ㅣ가-힣]/g.test(inputValue)) {
+    return '한국어';
+  } else {
+    return true;
+  }
+};
+
+const Form = ({ makeMeme, errorMessage }) => {
+  const [inputValue, setInputValue] = React.useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (characterValidation(inputValue) !== true) {
+      errorMessage(characterValidation(inputValue));
+      return;
+    }
+    makeMeme(inputValue);
+  };
   return (
     <div>
-      <h1>Test</h1>
+      <form action="" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Create a meme "
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+const App = () => {
+  const [error, setError] = React.useState('');
+  const [catImage, setCatImage] = React.useState('');
+  const makeMeme = async (inputValue) => {
+    const catMeme = await fetchAPI(inputValue);
+    setCatImage(() => catMeme);
+  };
+  const errorMessage = (validation) => {
+    setError((prev) => validation);
+  };
+  return (
+    <div>
+      <h1>Cat Meme Generator</h1>
+      <p className="input_error">
+        {error ? `Error : ${error} is not supported` : null}
+      </p>
+      <Form makeMeme={makeMeme} errorMessage={errorMessage} />
+      <img className="cat_main_image" src={catImage} alt="" />
     </div>
   );
 };
